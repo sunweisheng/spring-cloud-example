abd149ef571eb3f8f390d9d45ec87b0e9769ecbf
sunweisheng <sunweisheng@live.cn>
2019-09-27 02:26:32 +0800

    总线机制刷新配置信息

--- spring-cloud-config-server/src/main/java/com/bluersw/config/server/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/bluersw/config/server/SpringCloudConfigServerApplication.java
@@ -2,6 +2,7 @@ package com.bluersw.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
@@ -10,6 +11,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 //启动服务发现
 @EnableDiscoveryClient
+@ServletComponentScan
 public class SpringCloudConfigServerApplication {
 
 	public static void main(String[] args) {
