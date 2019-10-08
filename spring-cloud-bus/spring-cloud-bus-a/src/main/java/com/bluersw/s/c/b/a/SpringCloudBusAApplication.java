bb56335f7eface28dd64027ed76f2bfe7c8ed3c5
sunweisheng <sunweisheng@live.cn>
2019-10-08 23:33:50 +0800

    完成Spring Cloud SL示例

--- spring-cloud-bus/spring-cloud-bus-a/src/main/java/com/bluersw/s/c/b/a/SpringCloudBusAApplication.java
+++ spring-cloud-bus/spring-cloud-bus-a/src/main/java/com/bluersw/s/c/b/a/SpringCloudBusAApplication.java
@@ -1,9 +1,14 @@
 package com.bluersw.s.c.b.a;
 
+import com.bluersw.s.c.b.sl.PrivateChatRemoteApplicationEvent;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.bus.jackson.RemoteApplicationEventScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(value = "com.bluersw")
 public class SpringCloudBusAApplication {
 
 	public static void main(String[] args) {
--- spring-cloud-bus/spring-cloud-bus-shared-library/src/main/java/com/bluersw/s/c/b/sl/ListenerConfiguration.java
+++ spring-cloud-bus/spring-cloud-bus-shared-library/src/main/java/com/bluersw/s/c/b/sl/ListenerConfiguration.java
@@ -1,11 +1,13 @@
 package com.bluersw.s.c.b.sl;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.cloud.bus.jackson.RemoteApplicationEventScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConditionalOnClass(PrivateChatListener.class)
+@RemoteApplicationEventScan(basePackageClasses=PrivateChatRemoteApplicationEvent.class)
 public class ListenerConfiguration {
 
 	@Bean
